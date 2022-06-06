87031e065874b2e2ed8279c3e2cd43ccfa4abad6
PeiYun <ollylee8520@gmail.com>
2022-06-06 17:59:28 -0600

    change started code html/css/add card bg change js, connect get/delet api and rewirte to db data

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -1,3 +1,96 @@
+// const { nanoid } = require("nanoid");
+// const moment = require('moment');
+//note change bg js
+const note = document.querySelector('.right');
+const grayBtn = document.querySelector('.circle.gray');
+const pinkBtn = document.querySelector('.pink');
+const yellowBtn = document.querySelector('.yellow');
+const greenBtn = document.querySelector('.green');
+const whiteBtn = document.querySelector('.white');
+const inputEl = document.querySelector(".form-control")
+const textareaEl = document.querySelector("textarea");
+
+const borderCancel =()=>{
+    const circleEl = document.querySelectorAll('.circle');
+    for (var i = 0; i < circleEl.length; i++) {
+        circleEl[i].style.boxShadow = "none";
+        }        
+    }
+
+const changeGray =()=>{
+    note.style.backgroundColor = "gray";
+    inputEl.style.backgroundColor = "gray";
+    inputEl.classList.add('placeholder-white');
+    textareaEl.style.backgroundColor = "gray";
+    textareaEl.classList.add('placeholder-white');
+    textareaEl.style.color = "white";
+    borderCancel();
+    grayBtn.style.boxShadow = "0 0 0 0.2rem rgb(0 123 255 / 25%";
+}
+
+
+const changeYellow =()=>{
+    note.style.backgroundColor = "#F6CD45";
+    inputEl.style.backgroundColor = "#F6CD45";
+    inputEl.classList.add('placeholder-white');
+    inputEl.style.color = "white";
+    textareaEl.style.backgroundColor = "#F6CD45";
+    textareaEl.classList.add('placeholder-white');
+    textareaEl.style.color = "white";
+    borderCancel();
+    yellowBtn.style.boxShadow = "0 0 0 0.2rem rgb(0 123 255 / 25%";
+}
+
+const changePink =()=>{
+    note.style.backgroundColor = "#D99B9D";
+    inputEl.style.backgroundColor = "#D99B9D";
+    inputEl.classList.add('placeholder-white');
+    inputEl.style.color = "white";
+    textareaEl.style.backgroundColor = "#D99B9D";
+    textareaEl.classList.add('placeholder-white');
+    textareaEl.style.color = "white";
+    borderCancel();
+    pinkBtn.style.boxShadow = "0 0 0 0.2rem rgb(0 123 255 / 25%";
+}
+
+const changeGreen =()=>{
+    note.style.backgroundColor = "#5ECA8C";
+    inputEl.style.backgroundColor = "#5ECA8C";
+    inputEl.classList.add('placeholder-white');
+    inputEl.style.color = "white";
+    textareaEl.style.backgroundColor = "#5ECA8C";
+    textareaEl.classList.add('placeholder-white');
+    textareaEl.style.color = "white";
+    borderCancel();
+    greenBtn.style.boxShadow = "0 0 0 0.2rem rgb(0 123 255 / 25%";
+}
+
+const changeWhite =()=>{
+    note.style.backgroundColor = "white";
+    inputEl.style.backgroundColor = "white";
+    inputEl.classList.remove('placeholder-white');
+    inputEl.style.color = "black";
+    textareaEl.style.backgroundColor = "white";
+    textareaEl.classList.remove('placeholder-white');
+    textareaEl.style.color = "black";
+    borderCancel();
+    whiteBtn.style.boxShadow = "0 0 0 0.2rem rgb(0 123 255 / 25%";
+}
+grayBtn.addEventListener('click', changeGray);
+pinkBtn.addEventListener('click', changePink);
+yellowBtn.addEventListener('click', changeYellow);
+greenBtn.addEventListener('click', changeGreen);
+whiteBtn.addEventListener('click', changeWhite);
+
+// const submitData = () =>{
+//     // console.log(inputEl.value);
+//     // console.log(textareaEl.value);
+//     const data = {title:inputEl.value, text:textareaEl.value}
+//     console.log(data);
+// }
+
+//api
+
 let noteTitle;
 let noteText;
 let saveNoteBtn;
@@ -5,128 +98,133 @@ let newNoteBtn;
 let noteList;
 
 if (window.location.pathname === '/notes') {
-  noteTitle = document.querySelector('.note-title');
-  noteText = document.querySelector('.note-textarea');
-  saveNoteBtn = document.querySelector('.save-note');
-  newNoteBtn = document.querySelector('.new-note');
-  noteList = document.querySelectorAll('.list-container .list-group');
+    noteTitle = document.querySelector('.note-title');
+    noteText = document.querySelector('.note-textarea');
+    saveNoteBtn = document.querySelector('.save-note');
+    newNoteBtn = document.querySelector('.new-note');
+    noteList = document.querySelectorAll('.list-container .list-group');
 }
 
 // Show an element
 const show = (elem) => {
-  elem.style.display = 'inline';
+    elem.style.display = 'inline';
 };
 
 // Hide an element
 const hide = (elem) => {
-  elem.style.display = 'none';
+    elem.style.display = 'none';
 };
 
-// activeNote is used to keep track of the note in the textarea
 let activeNote = {};
 
 const getNotes = () =>
-  fetch('/api/notes', {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
+    fetch('/api/notes', {
+        method: 'GET',
+        headers: {
+        'Content-Type': 'application/json',
+        },
+    });
 
 const saveNote = (note) =>
-  fetch('/api/notes', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(note),
-  });
+    fetch('/api/notes', {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(note),
+    });
 
 const deleteNote = (id) =>
-  fetch(`/api/notes/${id}`, {
-    method: 'DELETE',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
+    fetch(`/api/notes/${id}`, {
+        method: 'DELETE',
+        headers: {
+        'Content-Type': 'application/json',
+        }
+    });
 
 const renderActiveNote = () => {
-  hide(saveNoteBtn);
+    hide(saveNoteBtn);
 
-  if (activeNote.id) {
-    noteTitle.setAttribute('readonly', true);
-    noteText.setAttribute('readonly', true);
-    noteTitle.value = activeNote.title;
-    noteText.value = activeNote.text;
-  } else {
-    noteTitle.removeAttribute('readonly');
-    noteText.removeAttribute('readonly');
-    noteTitle.value = '';
-    noteText.value = '';
-  }
+    if (activeNote.id) {
+        noteTitle.setAttribute('readonly', true);
+        noteText.setAttribute('readonly', true);
+        noteTitle.value = activeNote.title;
+        noteText.value = activeNote.text;
+    } else {
+        noteTitle.removeAttribute('readonly');
+        noteText.removeAttribute('readonly');
+        noteTitle.value = '';
+        noteText.value = '';
+    }
 };
 
 const handleNoteSave = () => {
-  const newNote = {
-    title: noteTitle.value,
-    text: noteText.value,
-  };
-  saveNote(newNote).then(() => {
-    getAndRenderNotes();
-    renderActiveNote();
-  });
+    // const CurrentDate = moment().format('YYYY/MM/DD');
+    console.log("handleNoteSave");
+    const newNote = {
+        title: noteTitle.value,
+        text: noteText.value,
+        id: Math.random().toString(16).slice(2)
+        // id: nanoid(),
+        // date:CurrentDate
+    };
+    console.log("newNote",newNote);
+    saveNote(newNote).then(() => {
+        getAndRenderNotes();
+        renderActiveNote();
+    });
 };
 
 // Delete the clicked note
 const handleNoteDelete = (e) => {
   // Prevents the click listener for the list from being called when the button inside of it is clicked
-  e.stopPropagation();
+    e.stopPropagation();
 
-  const note = e.target;
-  const noteId = JSON.parse(note.parentElement.getAttribute('data-note')).id;
+    const note = e.target;
+    const noteId = JSON.parse(note.parentElement.getAttribute('data-note')).id;
 
-  if (activeNote.id === noteId) {
-    activeNote = {};
-  }
+    if (activeNote.id === noteId) {
+        activeNote = {};
+    }
 
-  deleteNote(noteId).then(() => {
-    getAndRenderNotes();
-    renderActiveNote();
-  });
+    deleteNote(noteId).then(() => {
+        getAndRenderNotes();
+        renderActiveNote();
+    });
 };
 
 // Sets the activeNote and displays it
 const handleNoteView = (e) => {
-  e.preventDefault();
-  activeNote = JSON.parse(e.target.parentElement.getAttribute('data-note'));
-  renderActiveNote();
+    e.preventDefault();
+    activeNote = JSON.parse(e.target.parentElement.getAttribute('data-note'));
+    renderActiveNote();
 };
 
 // Sets the activeNote to and empty object and allows the user to enter a new note
 const handleNewNoteView = (e) => {
-  activeNote = {};
-  renderActiveNote();
+    activeNote = {};
+    renderActiveNote();
 };
 
 const handleRenderSaveBtn = () => {
-  if (!noteTitle.value.trim() || !noteText.value.trim()) {
-    hide(saveNoteBtn);
-  } else {
-    show(saveNoteBtn);
-  }
+    if (!noteTitle.value.trim() || !noteText.value.trim()) {
+        hide(saveNoteBtn);
+    } else {
+        show(saveNoteBtn);
+    }
 };
 
 // Render the list of note titles
 const renderNoteList = async (notes) => {
-  let jsonNotes = await notes.json();
-  if (window.location.pathname === '/notes') {
-    noteList.forEach((el) => (el.innerHTML = ''));
-  }
+    let jsonNotes = await notes.json();
+    if (window.location.pathname === '/notes') {
+        noteList.forEach((el) => (el.innerHTML = ''));
+    }
 
-  let noteListItems = [];
+    let noteListItems = [];
 
-  // Returns HTML element with or without a delete button
-  const createLi = (text, delBtn = true) => {
+    // Returns HTML element with or without a delete button
+    const createLi = (text, delBtn = true) => {
     const liEl = document.createElement('li');
     liEl.classList.add('list-group-item');
 
@@ -138,46 +236,45 @@ const renderNoteList = async (notes) => {
     liEl.append(spanEl);
 
     if (delBtn) {
-      const delBtnEl = document.createElement('i');
-      delBtnEl.classList.add(
-        'fas',
-        'fa-trash-alt',
-        'float-right',
-        'text-danger',
-        'delete-note'
-      );
-      delBtnEl.addEventListener('click', handleNoteDelete);
+        const delBtnEl = document.createElement('i');
+        delBtnEl.classList.add(
+            'fas',
+            'fa-trash-alt',
+            'float-right',
+            'text-danger',
+            'delete-note'
+        );
+        delBtnEl.addEventListener('click', handleNoteDelete);
 
-      liEl.append(delBtnEl);
+        liEl.append(delBtnEl);
     }
 
     return liEl;
-  };
+    };
 
-  if (jsonNotes.length === 0) {
-    noteListItems.push(createLi('No saved Notes', false));
-  }
+    if (jsonNotes.length === 0) {
+        noteListItems.push(createLi('No saved Notes', false));
+    }
 
-  jsonNotes.forEach((note) => {
-    const li = createLi(note.title);
-    li.dataset.note = JSON.stringify(note);
+    jsonNotes.forEach((note) => {
+        const li = createLi(note.title);
+        li.dataset.note = JSON.stringify(note);
+        noteListItems.push(li);
+    });
 
-    noteListItems.push(li);
-  });
-
-  if (window.location.pathname === '/notes') {
-    noteListItems.forEach((note) => noteList[0].append(note));
-  }
+    if (window.location.pathname === '/notes') {
+        noteListItems.forEach((note) => noteList[0].append(note));
+    }
 };
 
 // Gets notes from the db and renders them to the sidebar
 const getAndRenderNotes = () => getNotes().then(renderNoteList);
 
 if (window.location.pathname === '/notes') {
-  saveNoteBtn.addEventListener('click', handleNoteSave);
-  newNoteBtn.addEventListener('click', handleNewNoteView);
-  noteTitle.addEventListener('keyup', handleRenderSaveBtn);
-  noteText.addEventListener('keyup', handleRenderSaveBtn);
+    saveNoteBtn.addEventListener('click', handleNoteSave);
+    newNoteBtn.addEventListener('click', handleNewNoteView);
+    noteTitle.addEventListener('keyup', handleRenderSaveBtn);
+    noteText.addEventListener('keyup', handleRenderSaveBtn);
 }
 
 getAndRenderNotes();
--- Develop/server.js
+++ Develop/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
+const fs = require('fs');
 const path = require('path');
 const { clog } = require('./middleware/clog');
-
+const dbData = require("./db/db.json");
 // const api = require('./routes/index.js');
 const app = express();
-
 
 const PORT = process.env.port || 3001;
 
@@ -13,17 +13,49 @@ app.use(clog);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // app.use('/', api);
-// app.use(express.static('../public'));
+app.use(express.static('./public'));
 
 app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, './public/index.html'))
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.get('/notes', (req, res) => {
     console.info(`${req.method} request received for notes`)
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
+    res.sendFile(path.join(__dirname, './public/notes.html'))
+});
+
+app.get('/api/notes', (req, res) => {
+    console.info(`${req.method} request received for notes`)
+    res.json(dbData)
+});
+
+app.post('/api/notes', (req, res) => {
+    console.info("update data in server",req.body);
+    const newdata = [...dbData, req.body];
+    fs.writeFile(`./db/db.json`, JSON.stringify(newdata), (err) =>
+        err
+            ? console.error(err)
+            : console.log(
+                `db data has been written to JSON file`
+            )
+    );
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+    console.log("delete data in server");
+    console.log("req.params.id",req.params.id);
+    const idToRemove = req.params.id;
+    const removeItem = dbData.filter((item) => item.id !== idToRemove);
+    console.log("removeItem",removeItem);
+    fs.writeFile(`./db/db.json`, JSON.stringify(removeItem), (err) =>
+        err
+            ? console.error(err)
+            : console.log(
+                `db data has been written to JSON file`
+            )
+    );
 });
 
 app.listen(PORT, () =>
-    console.log(`App listening at http://localhost:${PORT} 🚀`)
+    console.log(`App listening at http://localhost:${PORT}/notes 🚀`)
 );
