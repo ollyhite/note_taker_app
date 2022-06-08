bba89402aedfea8f498c93cc6eb2c4d1c86cdb93
PeiYun <ollylee8520@gmail.com>
2022-06-08 14:49:25 -0600

    push to heroku

--- Develop/helpers/moment.js
+++
@@ -1,8 +0,0 @@
-const moment = require("moment");
-
-const CurrentDate = () =>{
-    const currentDate = moment().format('MM/DD/YYYY');
-    return currentDate;
-}
-
-module.exports = { CurrentDate };
